4f35f09bd80f1bde98cc9bfa1319e5396047b78c
Abinash Karmakar <codeAbinash@gmail.com>
2024-02-01 22:17:17 +0530

    Refactor list items in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import './App.css'
-
 function App() {
   return (
     <div className='min-h-[100dvh] bg-gradient-to-t from-accent-light/50 to-accent-light/90 bg-fixed pb-10'>
@@ -21,9 +19,9 @@ function App() {
       <div className='px-4'>
         <Nav />
         <div className='grid gap-2'>
-          <ListItem />
-          <ListItem />
-          <ListItem />
+          <ListItem1 />
+          <ListItem2 />
+          <ListItem3 />
           <ListItem />
           <ListItem />
           <ListItem />
@@ -50,6 +48,69 @@ function Nav() {
   )
 }
 
+function ListItem1() {
+  return (
+    <div className='flex grow items-center justify-between rounded-[1.2rem] border-accent bg-blue-400 p-2 px-3 font-medium'>
+      <div className='flex items-center justify-around gap-3'>
+        <div className='flex aspect-square items-center justify-center rounded-full bg-blue-500 px-3 text-center text-sm font-medium'>
+          1
+        </div>
+        <img src='https://picsum.photos/50' alt='' className='aspect-square w-9 rounded-full bg-blue-500' />
+        <div className='text-sm text-black/70'>123456789</div>
+      </div>
+      <div></div>
+      <div className='flex items-center justify-around gap-4'>
+        <img src='trophy2.jpg' alt='' className='w-5 mix-blend-multiply' />
+        <div className='flex items-center justify-center gap-1 rounded-full bg-blue-500 px-2 py-1 text-xs font-bold text-black/80 shadow-md shadow-accent-dark/20'>
+          <Coin /> 1900.0
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function ListItem2() {
+  return (
+    <div className='flex grow items-center justify-between rounded-[1.2rem] border-accent bg-green-400 p-2 px-3 font-medium text-gray-900'>
+      <div className='flex items-center justify-around gap-3'>
+        <div className='flex aspect-square items-center justify-center rounded-full bg-green-500 px-3 text-center text-sm font-medium text-black/70'>
+          1
+        </div>
+        <img src='https://picsum.photos/50' alt='' className='aspect-square w-9 rounded-full bg-blue-500' />
+        <div className='text-sm text-black/70'>123456789</div>
+      </div>
+      <div></div>
+      <div className='flex items-center justify-around gap-4'>
+        <img src='trophy2.jpg' alt='' className='w-5 mix-blend-multiply' />
+        <div className='flex items-center justify-center gap-1 rounded-full bg-green-500 px-2 py-1 text-xs font-bold text-black/80 shadow-md shadow-accent-dark/20'>
+          <Coin /> 1900.0
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function ListItem3() {
+  return (
+    <div className='flex grow items-center justify-between rounded-[1.2rem] border-accent bg-red-400 p-2 px-3 font-medium text-gray-900'>
+      <div className='flex items-center justify-around gap-3'>
+        <div className='flex aspect-square items-center justify-center rounded-full bg-red-500 px-3 text-center text-sm font-medium text-black/70'>
+          1
+        </div>
+        <img src='https://picsum.photos/50' alt='' className='aspect-square w-9 rounded-full bg-blue-500' />
+        <div className='text-sm text-black/70'>123456789</div>
+      </div>
+      <div></div>
+      <div className='flex items-center justify-around gap-4'>
+        <img src='trophy2.jpg' alt='' className='w-5 mix-blend-multiply' />
+        <div className='flex items-center justify-center gap-1 rounded-full bg-red-500 px-2 py-1 text-xs font-bold text-black/80 shadow-md shadow-accent-dark/20'>
+          <Coin /> 1900.0
+        </div>
+      </div>
+    </div>
+  )
+}
+
 function ListItem() {
   return (
     <div className='flex grow items-center justify-between rounded-[1.2rem] border border-accent bg-accent-light p-2 px-3 font-medium text-gray-900'>
@@ -63,7 +124,7 @@ function ListItem() {
       <div></div>
       <div className='flex items-center justify-around gap-4'>
         <img src='trophy2.jpg' alt='' className='w-5 mix-blend-multiply' />
-        <div className='flex items-center justify-center gap-1 rounded-full bg-accent px-2 py-1 text-xs text-black/80 shadow-md shadow-accent-dark/20'>
+        <div className='flex items-center justify-center gap-1 rounded-full bg-accent px-2 py-1 text-xs font-bold text-black/80 shadow-md shadow-accent-dark/20'>
           <Coin /> 1900.0
         </div>
       </div>
@@ -76,7 +137,7 @@ function Card({ img }: { img?: string }) {
     <div className='flex aspect-[3/4] w-full flex-col items-center justify-around rounded-2xl border border-accent-dark bg-accent py-2 shadow-xl shadow-accent/30'>
       <img src={img} alt='' className='w-10' />
       <div className='text-sm font-semibold text-black/80'>Ananta</div>
-      <div className='flex items-center justify-center gap-1 rounded-full border border-accent-dark/40 bg-accent-light px-2 py-1 text-xs font-medium text-black/80 shadow-md shadow-accent-dark/20'>
+      <div className='flex items-center justify-center gap-1 rounded-full border border-accent-dark/40 bg-accent-light px-2 py-1 text-xs font-bold text-black/80 shadow-md shadow-accent-dark/20'>
         <Coin /> 1000.0
       </div>
     </div>
