3160e41a8565c853e384458ba2a8993bb51d6740
Abinash Karmakar <codeAbinash@gmail.com>
2024-02-01 20:08:01 +0530

    Refactor UI layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,31 @@ import './App.css'
 
 function App() {
   return (
-    <div className='h-[100dvh] bg-gradient-to-t from-accent-light/50 to-accent-light/90 px-2 pb-5'>
-      <div className='grid gap-4 pt-5'>
-        <div>
-          <p className='text-center text-2xl font-bold text-black/80'>Leatherboard</p>
-          <p className='text-center text-lg font-medium'>Last 3 Winners</p>
+    <div className='min-h-[100dvh] bg-gradient-to-t from-accent-light/50 to-accent-light/90 bg-fixed pb-10'>
+      <div className='pt-3'>
+        <p className='text-center text-xl font-bold text-black/80'>Leatherboard</p>
+        <p className='text-center text-base font-medium'>Last 3 Winners</p>
+      </div>
+      <div className='mt-5 flex w-full gap-3 px-3'>
+        <div className='mt-10 w-full'>
+          <Card img='2nd.png' />
         </div>
-        <div className='mt-5 flex h-[25dvh] w-full items-center px-2'>
-          <div className='flex h-full flex-grow flex-col items-start justify-end'>
-            <Card img='2nd.png' />
-          </div>
-          <div className='flex h-full flex-grow flex-col items-center justify-start'>
-            <Card img='1st.png' />
-          </div>
-          <div className='flex h-full flex-grow flex-col items-end justify-end'>
-            <Card img='3rd.png' />
-          </div>
+        <div className='w-full'>
+          <Card img='1st.png' />
+        </div>
+        <div className='mt-10 w-full'>
+          <Card img='3rd.png' />
         </div>
       </div>
-      <div className='p-3'>
+      <div className='px-4'>
         <Nav />
-        <div className='grid gap-3'>
+        <div className='grid gap-2'>
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
           <ListItem />
           <ListItem />
           <ListItem />
@@ -37,7 +41,7 @@ export default App
 
 function Nav() {
   return (
-    <div className='flex items-center justify-around pb-5 pt-6 text-sm font-semibold'>
+    <div className='flex items-center justify-around pb-3 pt-6 text-sm font-semibold'>
       <div className='active w-[45%] border-b-[3px] border-accent-dark py-2 text-center text-black/80 shadow-accent-light/70'>
         Top Winnings
       </div>
@@ -48,7 +52,7 @@ function Nav() {
 
 function ListItem() {
   return (
-    <div className='flex grow items-center justify-between rounded-[1.2rem] border border-accent bg-accent-light p-4 font-medium text-gray-900'>
+    <div className='flex grow items-center justify-between rounded-[1.2rem] border border-accent bg-accent-light p-2 px-3 font-medium text-gray-900'>
       <div className='flex items-center justify-around gap-3'>
         <div className='flex aspect-square items-center justify-center rounded-full bg-accent px-3 text-center text-sm font-medium text-black/70'>
           1
@@ -59,8 +63,8 @@ function ListItem() {
       <div></div>
       <div className='flex items-center justify-around gap-4'>
         <img src='trophy2.jpg' alt='' className='w-5 mix-blend-multiply' />
-        <div className='rounded-full bg-accent px-3 py-1 text-xs text-black/80 shadow-md shadow-accent-dark/20'>
-          1900.0
+        <div className='flex items-center justify-center gap-1 rounded-full bg-accent px-2 py-1 text-xs text-black/80 shadow-md shadow-accent-dark/20'>
+          <Coin /> 1900.0
         </div>
       </div>
     </div>
@@ -69,15 +73,16 @@ function ListItem() {
 
 function Card({ img }: { img?: string }) {
   return (
-    <div
-      className='flex aspect-[2/3] w-[92%] flex-col items-center justify-around rounded-2xl border border-accent-dark bg-accent py-2 shadow-xl shadow-accent/30
-    '
-    >
-      <img src={img} alt='' className='w-10 ' />
+    <div className='flex aspect-[3/4] w-full flex-col items-center justify-around rounded-2xl border border-accent-dark bg-accent py-2 shadow-xl shadow-accent/30'>
+      <img src={img} alt='' className='w-10' />
       <div className='text-sm font-semibold text-black/80'>Ananta</div>
-      <div className='rounded-full border border-accent-dark/40 bg-accent-light px-3 py-1 text-xs font-medium text-black/80 shadow-md shadow-accent-dark/20'>
-        1000.0
+      <div className='flex items-center justify-center gap-1 rounded-full border border-accent-dark/40 bg-accent-light px-2 py-1 text-xs font-medium text-black/80 shadow-md shadow-accent-dark/20'>
+        <Coin /> 1000.0
       </div>
     </div>
   )
+}
+
+function Coin() {
+  return <img src='coin.png' alt='' className='h-4 w-4' />
 }
